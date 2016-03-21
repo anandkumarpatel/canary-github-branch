30d67e83753643b0bea1f85c7de882fa3557fc9e
thejsj <jorge.silva@thejsj.com>
2016-03-21 13:17:45 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import http from 'http'
+
+const PORT = 8000
+
+function handleRequest (request, response) {
+  response.end('It Works!! Path Hit: ' + request.url);
+}
+
+let server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+  console.log("Server listening on: http://localhost:%s", PORT);
+});
